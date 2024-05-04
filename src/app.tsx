415f6e8e56cd2197d6a1323d43390710eaebbb1d
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-05-04 20:57:38 -0300

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/header";
 import { Tabs } from "./components/tabs";
 import { Button } from "./components/ui/button";
 import { Control, Input } from "./components/ui/input";
+// import { Pagination } from "./components/pagination";
 import {
   Table,
   TableBody,
@@ -11,9 +12,40 @@ import {
   TableHeader,
   TableRow,
 } from "./components/ui/table";
+import { useQuery } from "@tanstack/react-query";
 
+export interface TagResponse {
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
+}
+
+export interface Tag {
+  title: string;
+  slug: string;
+  amountOfVideos: number;
+  id: string;
+}
 
 export function App() {
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ["get-tags"],
+    queryFn: async () => {
+      const response = await fetch(
+        "http://localhost:3333/tags?_page=1&_per_page=10"
+      );
+      const data = await response.json();
+
+      return data;
+    },
+  });
+
+  if (isLoading) return null;
+
   return (
     <>
       <div className="py-10 space-y-8">
@@ -50,18 +82,18 @@ export function App() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {Array.from({ length: 10 }).map((value, index) => {
+              {tagsResponse?.data.map((tag) => {
                 return (
-                  <TableRow key={index}>
+                  <TableRow key={tag.id}>
                     <TableCell>
                       <div className="flex flex-col gap-0.5">
-                        <span className="font-medium">React</span>
-                        <span className="text-xs text-zinc-500">
-                          faba4daa-f7ef-483f-82ab-20f05d3d0de1
-                        </span>
+                        <span className="font-medium">{tag.title}</span>
+                        <span className="text-xs text-zinc-500">{tag.id}</span>
                       </div>
                     </TableCell>
-                    <TableCell className="text-zinc-300">13 video(s)</TableCell>
+                    <TableCell className="text-zinc-300">
+                      {tag.amountOfVideos} video(s)
+                    </TableCell>
                     <TableCell className="text-right">
                       <Button size="icon">
                         <MoreHorizontal className="size-4" />
@@ -72,6 +104,7 @@ export function App() {
               })}
             </TableBody>
           </Table>
+          {/* <Pagination pages={0} items={0} page={0} /> */}
         </main>
       </div>
     </>
